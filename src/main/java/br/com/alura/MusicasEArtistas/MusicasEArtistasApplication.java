47f8e93da59210f669fcc99864edfb77f560e8eb
Luiz Ferreira <luhaferr@gmail.com>
2024-10-29 17:24:02 -0300

    first commit

---
+++ src/main/java/br/com/alura/MusicasEArtistas/MusicasEArtistasApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.MusicasEArtistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicasEArtistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicasEArtistasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/MusicasEArtistas/MusicasEArtistasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.MusicasEArtistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicasEArtistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
